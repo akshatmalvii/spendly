f9075dee1ab24e0c229876d5bb3c4f8e8ca35d1e
akshatmalvii <malviakshat2001@gmail.com>
2025-09-09 23:19:18 +0530

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-white text-black dark:bg-black dark:text-white">
+      Home page 
+    </div>
+  );
+}
