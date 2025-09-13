1508f6f39ef16ddb7610952d23ccf12667336842
akshatmalvii <malviakshat2001@gmail.com>
2025-09-13 14:09:30 +0530

    Add Navbar component and guest, about, contact pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-import Image from "next/image";
+import Guest from '@/compnonents/Guest';
+import { currentUser } from '@clerk/nextjs/server';
 
-export default function Home() {
+
+export default async function Home() {
+  const user = await currentUser();
+    if (!user) {
+    return <Guest />;
+  }
   return (
     <div className="bg-white text-black dark:bg-black dark:text-white">
       Home page 
