488fe966b661deb766788fa5758c48287b4d6d63
akshatmalvii <malviakshat2001@gmail.com>
2025-09-13 23:23:20 +0530

    Add expense functionality

---
+++ compnonents/AIInsights.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AIInsights() {
+  return (
+    <div>AIInsights</div>
+  )
+}
+
+export default AIInsights
---
+++ compnonents/RecordItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RecordItem() {
+  return (
+    <div>RecordItem</div>
+  )
+}
+
+export default RecordItem
